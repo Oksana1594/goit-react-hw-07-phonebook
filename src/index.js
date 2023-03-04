a2be1cd43509a48ee3c1f7eabe1dd9ccfdd2bd23
Oksana1594 <106923229+Oksana1594@users.noreply.github.com>
2023-03-04 11:06:59 +0300

    add changes to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-// import { PersistGate } from 'redux-persist/integration/react';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'App';
 import './index.css';
-// import { rootReducer } from 'redux/store';
 import store from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
