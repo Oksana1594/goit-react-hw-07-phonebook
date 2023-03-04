f0a14cc9110ddd03b3b60b15f85bc39c680761b4
Oksana1594 <106923229+Oksana1594@users.noreply.github.com>
2023-03-04 11:02:00 +0300

    do HW

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+// import { PersistGate } from 'redux-persist/integration/react';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import App from 'App';
 import './index.css';
+// import { rootReducer } from 'redux/store';
+import store from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
